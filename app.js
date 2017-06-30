a4ba6c068993b0372197fb4c396b3d146f8b7136
Loic Talhouarne <talhouar.loic@gmail.com>
2017-06-30 17:44:00 -0500

    more

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var nodemailer = require('nodemailer');
 var admin = require("firebase-admin");
 
 var serviceAccount = require("./leg-count-dev-firebase-adminsdk-8ocjf-c21fd6d9f7.json");
@@ -15,10 +15,28 @@ admin.initializeApp({
 var db = admin.database();
 var ref = db.ref("email");
 
-ref.once("value", function(snapshot) {
-  console.log(snapshot.val());
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'legcount@gmail.com',
+    pass: 'OMITTED'
+  }
 });
 
 ref.on("child_added", function(snapshot) {
-  console.log(snapshot.val());
+  if(snapshot.val().status === 'notSent') {
+    transporter.sendMail({
+        from: 'legcount@gmail.com',
+        to: snapshot.val().to,
+        subject: 'You are invited to join ' + snapshot.val().teamName + ' team',
+        text: 'Please go to legcount.com to sign up for your team.'
+    }, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+    ref.child(snapshot.key).update({"status" : "sent"});
+  }
 });
